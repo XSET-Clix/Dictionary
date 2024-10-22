251c1838de95df9865b4d3f695626723fa3a8c5a
XSET-Clix <shahded2124@gmail.com>
2024-10-21 20:34:47 -0400

    Add files via upload

---
+++ list&tuple.py
@@ -0,0 +1,28 @@
+#list
+fortnite = ["Battleroyale", "Reload", "Rocketracing"]
+print(fortnite)
+print(fortnite[2])
+fortnite.append("Superedvsblue")
+print(fortnite)
+fortnite.append("Hoardrush")
+print(fortnite)
+#tuple
+gamer = ("Dhruv", 11, 6, "paneer")
+print("My name is", gamer[0])
+print("My age is", gamer[1])
+print("My grade in school is", gamer[2])
+print("My favorite food is", gamer[3])
+#dictionary
+print("dictionary")
+fortnitepro = {"name": "Dhruv", "age": 11, "grade": 6, "food": "pizza"}
+print(fortnitepro["name"])
+print(fortnitepro["age"])
+print(fortnitepro["grade"])
+print(fortnitepro["food"])
+fortnitepro["country"]="USA"
+print(fortnitepro["country"])
+print(fortnitepro)
+fortnitepro["food"]="paneer"
+print(fortnitepro)
+del fortnitepro["grade"]
+print(fortnitepro)
